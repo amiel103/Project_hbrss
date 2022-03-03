4e0f75b4be8b2aacf6711256a60decede33da5ca
Cedric Matthew Verdida <cedricmatthew.verdida@fligno.com>
2022-03-03 22:36:46 +0800

    okay na ang server pag npm install lang utro

---
+++ plugins/moralis.client.js
@@ -0,0 +1,6 @@
+import { Moralis } from 'moralis';
+
+Moralis.start({ 
+    serverUrl: "https://tpophoaxlnt7.usemoralis.com:2053/server", 
+    appId: "R1zKL8ZFUu0EclVeaVtKZ96Y2pYPXzR6mrOg8wCC" 
+});
